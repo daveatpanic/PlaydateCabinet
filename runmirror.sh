1722a550f1b87d1b706a5b10e43e3317e903d153
Dave Hayden <dave@panic.com>
2025-03-31 11:31:23 -0500

    wait for A button to be pressed before going into mirror loop

--- runmirror.sh
+++ runmirror.sh
@@ -1,3 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 sudo python resetbutton.py &
-while true; do sudo mirror; done
+
+# wait for A button press (gpio 25) before starting mirror so that
+#  we can log in if we need to
+
+# pigs needs pigpiod running..
+sudo pigpiod
+pigs modes 25 r
+
+while true; do
+	if [[ $(pigs r 25) == "0" ]]; then break; fi
+	sleep 1
+done
+
+# ..but mirror doesn't work if it's running :eyeroll:
+sudo killall pigpiod
+
+# finally, loop mirror so it restart on crash/button reset
+while true; do sudo /home/pi/mirror; done
